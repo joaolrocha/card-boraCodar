c143da268b4edefb5f69dd6e72664a2b8c48bb90
joaolrocha <joaolrocha07@gmail.com>
2023-01-13 14:48:38 -0300

    :sparkles: Component Layout config

--- src/components/CardProduct/index.tsx
+++ src/components/CardProduct/index.tsx
@@ -1,15 +1,21 @@
+import sofa from '../../assets/image2.png'
 import ButtonCard from '../ButtonCard'
 import './styles.css'
 
+
 function CardProduct() {
   return (
-    <div className="card-container">
-      <div className="card-image">
-      </div>
-      <div className="card-info">
-        <h1>Sofá Margot II - Rosé</h1>
-        <h4>R$ 4.000</h4>
-        <ButtonCard />
+    <div className='home-container'>
+      <div className="card-container">
+        <div className="card-image">
+          <img src={sofa} alt="sofa" />
+        </div>
+        <div className="card-info">
+          <label>CÓDIGO: 42404</label>
+          <h1>Sofá Margot II - Rosé</h1>
+          <h4>R$ 4.000</h4>
+          <ButtonCard />
+        </div>
       </div>
     </div>
   )
