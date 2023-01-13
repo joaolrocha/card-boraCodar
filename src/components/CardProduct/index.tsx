6da32f118e2e216aa036ffe1fda5f50021fdec36
joaolrocha <joaolrocha07@gmail.com>
2023-01-13 12:09:20 -0300

    :sparkles: Project Created

---
+++ src/components/CardProduct/index.tsx
@@ -0,0 +1,18 @@
+import './styles.css'
+
+function CardProduct() {
+  return (
+    <div className="card-container">
+      <div className="card-image">
+      </div>
+      <div className="card-info">
+        <h1>Sofá Margot II - Rosé</h1>
+        <h4>R$ 4.000</h4>
+        <button typeof="clicked"> Adicionar a cesta</button>
+
+      </div>
+    </div>
+  )
+}
+
+export default CardProduct
